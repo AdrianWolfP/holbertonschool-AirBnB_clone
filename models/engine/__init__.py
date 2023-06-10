e28fda39b4ae94d9e15187827cfa917a98e13df7
Wolfgang Pendergass <119448680+AdrianWolfP@users.noreply.github.com>
2023-06-10 14:52:40 -0500

    added files in models, currently in engine

---
+++ models/engine/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for FileStorage class"""
+
+import json
+import os
+import models
+
+
+class FileStorage:
+    """class FileStorage serializes instances to
+    a JSON file and deserializes"""
