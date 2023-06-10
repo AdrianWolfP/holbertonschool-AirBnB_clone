3775321059cdf16e7235aec25d8f138a0ae85d10
Wolfgang Pendergass <119448680+AdrianWolfP@users.noreply.github.com>
2023-06-10 18:15:04 -0500

    wrote in wrong file for file storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Module for FileStorage class"""
+
+import json
+import os
+import models
+
+
+class FileStorage:
+    """class FileStorage serializes instances to
+    a JSON file and deserializes JSON file to instances:
+    private class attributes:
+    __file_path: (string) - path to JSON file
+    __objects: (dictionary) - empty nut will store all objects by <class name>.id"""
+    __file_path = "./file.json"
+    __objects = {}
+
+    @classmethod
+    def clear(cls):
+        FileStorage.__objects = {}
+
+    def all(self):
+        """ returns dictionary __objects"""
+        return self.__objects
+    
+    def new(self, obj):
+        """ set in __objects the obj with key <obj class name>.id """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """ serializes __objects to the JSON file (path: __file_path)"""
+        new_dict = {}
+        for key, value in self.__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(new_dict, f)
+    
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r') as f:
+                data = json.load(f)
+                for key in data:
+                    class_name = data[key]['__class__']
+                    obj_dict = data[key]
+                    cls = detattr(model, class_name)
+                    instance = cls(**obj_dict)
+                    self.__objects[key] = instance
+        except FileNotFoundError:
+            pass
